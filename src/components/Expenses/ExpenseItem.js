ab8639bababe629c6ee2dcf15c53f1b1f62226a8
Sumanth-MS <86466227+Sumanth-MS@users.noreply.github.com>
2022-03-17 13:41:39 +0530

    section 4 initian commit.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,10 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
@@ -9,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Clicked!</button>
     </div>
   );
 }
