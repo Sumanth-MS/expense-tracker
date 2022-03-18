2ef84c4d485e433faf0199f7946e894a4bdb6c38
Sumanth-MS <86466227+Sumanth-MS@users.noreply.github.com>
2022-03-18 17:20:53 +0530

    section 4 complete without filter expenses.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,9 @@
+import React from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  const clickHandler = () => {
-    console.log("clicked!");
-  };
+const ExpenseItem = (props) => {
 
   return (
     <div className="expense-item">
@@ -13,9 +12,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Clicked!</button>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,21 @@
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+// import ExpensesFilter from "./ExpensessFilter";
+import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
+  // const [filteredYear, setFilteredYear] = useState("2020");
+
+  // const filterChangeHandler = (selectedYear) => {
+  //   setFilteredYear(selectedYear);
+  // };
+
   return (
     <div className="expenses">
+      {/* <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      /> */}
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,6 +38,6 @@ function Expenses(props) {
       />
     </div>
   );
-}
+};
 
 export default Expenses;
